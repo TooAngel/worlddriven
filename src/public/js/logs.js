0089b5b9fad7fd153b28c31c7133d805ef5a7b52
Tobias Wilken <tooangel@tooangel.de>
2020-01-31 11:59:14 +0100

    Use websockets for the admin logs
    Create a websocket endpoint `/admin/logs` to be able to listen to logs.
    Provide an frontend to show these logs.
    
    Yes, this is public, that's the idea of worlddriven.

---
+++ src/public/js/logs.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+/**
+ * Application class
+ **/
+export class Logs extends React.Component { // eslint-disable-line no-unused-vars
+  /**
+   * contructor - The constructor
+   *
+   * @param {object} props - The properties
+   * @return {void}
+   **/
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataFromServer: [],
+    };
+    this.initWS = this.initWS.bind(this);
+  }
+
+  /**
+   * componentDidUpdate - When the component updated
+   *
+   * @return {void}
+   **/
+  componentDidUpdate() {
+    this.node.scrollTop = this.node.scrollHeight;
+  }
+
+  /**
+   * initWS - Initializes the websocket connection
+   *
+   * @return {void}
+   **/
+  initWS() {
+    this.ws = new WebSocket(`ws://localhost:8000/admin/logs`);
+    this.ws.onopen = () => {
+      console.log('connected');
+    };
+
+    this.ws.onmessage = (event) => {
+      const dataFromServer = this.state.dataFromServer;
+      dataFromServer.push(event.data);
+      if (dataFromServer.length > 100) {
+        dataFromServer.shift();
+      }
+      this.setState({dataFromServer: dataFromServer});
+    };
+
+    this.ws.onclose = () => {
+      console.log('disconnected');
+      this.initWS();
+    };
+  }
+
+  /**
+   * componentDidMount - on build of the component
+   *
+   * @return {void}
+   **/
+  componentDidMount() {
+    this.initWS();
+  }
+
+  /**
+   * render - renders
+   * @return {object} - The element to be renderd
+   **/
+  render() {
+    const lines = [];
+    for (let i=0; i<this.state.dataFromServer.length; i++) {
+      lines.push(<li key={i}>{this.state.dataFromServer[i]}</li>);
+    }
+    const ulStyle = {
+      listStyleType: 'none',
+      paddingLeft: '0px',
+    };
+    return <div className="log" ref={(node) => {
+      this.node = node;
+    }}><ul style={ulStyle}>{ lines }</ul></div>;
+  }
+}
